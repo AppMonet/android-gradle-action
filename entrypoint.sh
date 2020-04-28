e439a059054212246ea55ebffea77ee87bcc9ec1
Nick Jacob <nickbjacob@gmail.com>
2020-04-28 12:11:12 -0400

    absolute path FML

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,4 +10,4 @@ chmod +x ./gradlew
 infer run -- ./gradlew "$1" -x lint
 
 # collect the infra output & then write it to github as a check
-node /submit-report.js ./infer-out/report.json
+node /submit-report.js $PWD/infer-out/report.json
