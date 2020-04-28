cfa47903bad388ffdebb1356c9e6859d992ba495
Nick Jacob <nickbjacob@gmail.com>
2020-04-28 11:42:56 -0400

    fix entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,13 @@
 #!/bin/bash -l
 set -eu
 
+# copy this to the working directory also
+cp /local.properties .
+
 chmod +x ./gradlew
 
 # run the given target
 infer run -- ./gradlew "$1" -x lint
 
 # collect the infra output & then write it to github as a check
-node ./submit-report.js ./infer-out/report.json
+node /submit-report.js ./infer-out/report.json
