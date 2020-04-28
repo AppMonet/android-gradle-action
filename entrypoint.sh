ce1dd2a228e15c7b419baf7071a3d927967a07c4
Nick Jacob <nickbjacob@gmail.com>
2020-04-28 11:08:43 -0400

    entrypoint fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,14 +3,6 @@ set -eu
 
 chmod +x ./gradlew
 
-if [ -n "$1" ]; then
-    mkdir -p $ANDROID_HOME/licenses
-    echo -e $1 >> $ANDROID_HOME/licenses/android-sdk-license
-    echo $"\nLicences accepted"
-fi
-
-echo $"\n--> Running './gradlew $2'\n"
-
 # run the given target
 infer run -- ./gradlew "$1" -x lint
 
